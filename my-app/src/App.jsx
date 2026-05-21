ec92f56f618cc645579c7dc460f123f80247199f
Aarchi <aarchid28@gmail.com>
2026-05-21 10:18:19 +0530

    node files uploaded

---
+++ my-app/src/App.jsx
@@ -0,0 +1,21 @@
+//import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+//import viteLogo from './assets/vite.svg'
+//import heroImg from './assets/hero.png'
+//import './App.css'
+import Hello from "./Hello"
+import Bye from "./Bye"
+
+function App() {
+  return(
+    <>
+    <h1>App Component</h1>
+    <Hello />
+    <img src={reactLogo}  width="100px" height="100px"/>
+    <Bye />
+    </>
+  )
+  
+}
+
+export default App
---
+++ my-app/src/Hello.jsx
@@ -0,0 +1,6 @@
+import './App.css'
+function Hello(){
+    const name="aarchi"
+    return <h2 class="test">Hello {name}</h2>
+}
+export default Hello
